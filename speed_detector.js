ada6ddc6c38ecec1f34e1c78da103048723001d7
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-06-16 16:15:01 +0300

    added comments

--- speed_detector.js
+++ speed_detector.js
@@ -1,17 +1,25 @@
 let speed;
+// Declaring the speed limit
 const speedLimit = 70;
+
+// Declaring the points for demerit
 const pointsPerDemerit = 1;
 
+// To detect speed
 function speedDetector(speed){
     if (speed < 70){
         return "OK";
+    } else {
+      return calculateDemerit();
     }
 }
 
+// For calculating points due to demerit
 function calculateDemerit(speed){
  if(speed < speedLimit){
     return 0;
  } else if (speed > speedLimit){
+   // For calculating points
     const points = Math.floor((speed - speedLimit) / 5) * pointsPerDemerit;
     return points;
  } else if(points === 12){
